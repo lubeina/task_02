350fdb0f89fb9fe6d481f4a49ca8c98277a80d77
lubeina <52536093+lubeina@users.noreply.github.com>
2020-01-12 19:12:40 +0300

    Task02-1

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def home(request):
+    context = {
+        "title": "Home",
+        "msg": "Hello World!",
+    }
+    return render(request, 'homepage.html', context)
+
 # Create your views here.
